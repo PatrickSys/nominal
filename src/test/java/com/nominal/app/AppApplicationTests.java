99e400264220f57e2de9c437dc31e2292ee18f93
PatrickSys <rossellocolompatrick@gmail.com>
2021-04-28 23:36:09 +0200

    first commit

---
+++ src/test/java/com/nominal/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nominal.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
