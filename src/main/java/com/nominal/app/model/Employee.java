7c0ee0e34a08feb66f6253de0353a5d39cf8746b
mikeb4rt <rodrigosern@gmail.com>
2021-05-07 20:44:11 +0200

    CRUD employees

--- src/main/java/com/nominal/app/model/Employee.java
+++ src/main/java/com/nominal/app/model/Employee.java
@@ -1,2 +1,74 @@
-package com.nominal.app.model;public class Employee {
+package com.nominal.app.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Employee{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    int ID;
+    String dni;
+    String naf;
+    String professional_group;
+    int quotation_group;
+
+    public int getID() {
+        return ID;
+    }
+    public String getDni() {
+        return dni;
+    }
+    public String getNaf(){
+        return naf;
+    }
+
+    public String getProfessional_group() {
+        return professional_group;
+    }
+    public int getQuotation_group(){
+        return quotation_group;
+    }
+
+
+    // gets info from a Employee to be used in a query
+    public String toQueryInfo(){
+
+        return  "\"" + dni + "\",\"" +
+                naf + "\",\"" +
+                professional_group + "\", " +
+                quotation_group;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "ID=" + ID +
+                ", dni='" + dni + '\'' +
+                ", naf='" + naf + '\'' +
+                ", professional_group='" + professional_group + '\'' +
+                ", quotation_group=" + quotation_group +
+                '}';
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setNaf(String naf) {
+        this.naf = naf;
+    }
+
+    public void setProfessional_group(String professional_group) {
+        this.professional_group = professional_group;
+    }
+
+    public void setQuotation_group(int quotation_group) {
+        this.quotation_group = quotation_group;
+    }
 }
--- src/main/java/com/nominal/app/queries/EmployeeQueries.java
+++ src/main/java/com/nominal/app/queries/EmployeeQueries.java
@@ -1,2 +1,45 @@
-package com.nominal.app.queries;public class EmployeeQueries {
+package com.nominal.app.queries;
+
+import com.nominal.app.mapper.EmployeeMapper;
+import com.nominal.app.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/************************************************************************ยบ
+ Made by        Nominal Team
+ Date           05/05/2021
+ Package        com.nominal.app
+ Description:
+ ************************************************************************/
+
+@Component
+public class EmployeeQueries extends Queries{
+
+    @Autowired
+    final EmployeeMapper employeesMapper = new EmployeeMapper();
+
+    @Autowired
+    public EmployeeQueries() throws Exception {
+        super();
+
+    }
+    /**
+     * Creates a person object by mapping a resultset
+     * @param id the required employees id
+     * @return the object
+     * @throws SQLException raw sql ex
+     */
+    public Employee employeesFromQuery(int id) throws Exception {
+        Employee employee = new Employee();
+        String sql = "SELECT * FROM employees WHERE id = " + id;
+        System.err.println(sql);
+        ResultSet resultSet = executeQuery(sql);
+
+        if (resultSet.next()){
+            employee =  employeesMapper.mapRow(resultSet);
+        }
+        return employee;
+    }
 }
