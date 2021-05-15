0677189b30117d7f30a4e97c383ef94611cd913f
PatrickSys <rossellocolompatrick@gmail.com>
2021-05-16 00:12:50 +0200

    credentials deleted

--- src/main/java/com/nominal/app/DbConn.java
+++ src/main/java/com/nominal/app/DbConn.java
@@ -25,7 +25,7 @@ public class DbConn {
 
     public DbConn() throws Exception{
 
-        this.connection = DriverManager.getConnection("jdbc:mariadb://192.168.1.100:3306/nominal", "patrickdb", "adminpatrick");
+        this.connection = DriverManager.getConnection("jdbc:mariadb://90.162.223.55/nominal", "user", "password");
 
     }
 
